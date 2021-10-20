fe316226a4ce92979317024a46f2ef75994be6cc
fugacityengr <61220814+fugacityengr@users.noreply.github.com>
2021-10-20 15:50:34 +0800

    ✅ Add test for mapmaker

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,14 @@
-class Point():
-    pass
+# Module to create points that consist
+# of a city name,
+# latitude and longtitude coordinates
+from typing import Tuple
+
+
+class Point:
+    def __init__(self, name, latitude, longtitude) -> None:
+        self._name = name
+        self._longitude = longtitude
+        self._latitude = latitude
+
+    def get_lat_long(self) -> Tuple[int, int]:
+        return (self._latitude, self._longitude)
