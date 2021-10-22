73e98afd963e805e38327824c819561c56301337
fugacityengr <61220814+fugacityengr@users.noreply.github.com>
2021-10-22 14:35:44 +0800

    ✅ Parametrize test for stat per country

--- tests/chp3/video3/test_parametrize_start.py
+++ tests/chp3/video3/test_parametrize_start.py
@@ -6,7 +6,7 @@ from scripts import data_processor, data_aggregator
 
 @pytest.fixture(scope="module")
 def city_list_location():
-    return 'tests/resources/cities/'
+    return "tests/resources/cities/"
 
 
 @pytest.fixture(scope="module")
@@ -16,7 +16,7 @@ def process_data(city_list_location):
     def _specify_type(file_name_or_type):
         for f in files:
             if file_name_or_type in f:
-                if file_name_or_type != '.json':
+                if file_name_or_type != ".json":
                     data = data_processor.csv_reader(city_list_location + f)
                 else:
                     data = data_processor.json_reader(city_list_location + f)
@@ -25,15 +25,24 @@ def process_data(city_list_location):
     yield _specify_type
 
 
-def test_average_atitude_per_country(process_data):
+@pytest.mark.parametrize(
+    "country,stat,expected",
+    [
+        ("Andorra", "Mean", 1641.42),
+        ("Andorra", "Median", 1538.02),
+        ("Argentina", "Median", 125.0),
+    ],
+)
+def test_altitude_stat_per_country(process_data, country, stat, expected):
     data = process_data(file_name_or_type="clean_map.csv")
-    andorran_avg_res = data_aggregator.atitude_stat_per_country(data, 'Andorra', 'Mean')
+    andorran_avg_res = data_aggregator.atitude_stat_per_country(data, country, stat)
 
-    assert andorran_avg_res == {'Country': 'Andorra', 'Mean': 1641.42}
+    assert andorran_avg_res == {"Country": country, stat: expected}
 
 
-def test_median_atitude_per_country(process_data):
-    data = process_data(file_name_or_type="clean_map.csv")
-    andorran_median_res = data_aggregator.atitude_stat_per_country(data, 'Andorra', 'Median')
+# No longer needed as the above test is parametrized
+# def test_median_atitude_per_country(process_data):
+#     data = process_data(file_name_or_type="clean_map.csv")
+#     andorran_median_res = data_aggregator.atitude_stat_per_country(data, 'Andorra', 'Median')
 
-    assert andorran_median_res == {'Country': 'Andorra', 'Median': 1538.02}
+#     assert andorran_median_res == {'Country': 'Andorra', 'Median': 1538.02}
